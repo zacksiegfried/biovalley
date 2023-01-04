79a04597ffebf52e9583299cafbe0927bfefb2b7
Zack Siegfried <102556973+zacksiegfried@users.noreply.github.com>
2023-01-04 18:27:41 -0500

    execution fixes

--- microbiome_clustering/omeClust_demo.sh
+++ microbiome_clustering/omeClust_demo.sh
@@ -4,8 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DATA_PATH="$SCRIPT_DIR/data/dist_4_0.001_4_200.txt"
 META_PATH="$SCRIPT_DIR/data/truth_4_0.001_4_200.txt"
-OUTPUT_PATH"$SCRIPT_DIR/output"
+OUTPUT_PATH="$SCRIPT_DIR/output"
 
-echo(OUTPUT_PATH)
-
-omeClust -i DATA_PATH --metadata META_PATH -o OUTPUT_PATH --plot
+omeClust -i $DATA_PATH --metadata $META_PATH -o $OUTPUT_PATH --plot
